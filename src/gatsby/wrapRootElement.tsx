5ae5f22e9ae26612eccb6627bf7074305c1e90ad
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-06-16 05:29:46 +0500

    Removed unnecessary code

--- src/gatsby/wrapRootElement.tsx
+++ src/gatsby/wrapRootElement.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { WrapRootElementBrowserArgs } from "gatsby";
-// import { ApolloProvider } from "@apollo/client";
 import { ThemeProvider } from "@material-ui/core";
 
-// import { IdentityProvider } from "../context/netlifyIdentityContext";
-// import { apolloClient } from "../context/apolloClient";
 import AmplifyClient from "../context/amplifyClient";
 import { AuthProvider } from "../context/authContext";
 import { customMuiTheme } from "../context/muiTheme";
--- src/types/types.ts
+++
@@ -1,5 +0,0 @@
-export type Todo = {
-  id: string;
-  content: string;
-  status: boolean;
-};
